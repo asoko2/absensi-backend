f4602c8e6d4c33157baa050b4c3e5ea3e2a8c9fc
Anang Hariyanto N <sokoanang@gmail.com>
2023-10-16 17:11:11 +0700

    update courses model

--- app/models/courses.model.js
+++ app/models/courses.model.js
@@ -1,5 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const Courses = sequelize.define("courses", {
+    course_code: {
+      type: DataTypes.STRING,
+      unique: true
+    },
     course_name: {
       type: DataTypes.STRING
     },
